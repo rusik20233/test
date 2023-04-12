156c3153e3127a2b6ebdfcf5c90a2bfc67551006
= <=>
2023-04-12 23:23:34 +0300

    sessiontwofull

---
+++ PushToGit/Views/ButtonsOverflow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PushToGit.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для ButtonsOverflow.xaml
+    /// </summary>
+    public partial class ButtonsOverflow : Page
+    {
+        public ButtonsOverflow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnUp_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new SignInPage());
+        }
+        private void BtnDown_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new WelcomePage());
+        }
+    }
+}
---
+++ PushToGit/Views/SignInPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using PushToGit.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PushToGit.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для SignInPage.xaml
+    /// </summary>
+    public partial class SignInPage : Page
+    {
+        public SignInPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            var currentUser = AppData.db.Users.FirstOrDefault(u => u.Logins == txbLogins.Text && u.Passwords == txbPasswords.Text);
+
+            if (currentUser != null)
+            {
+                NavigationService.Navigate(new WelcomePage());
+            }
+
+            else
+            {
+                MessageBox.Show("Неверные данные", "Ошибка входа");
+            }
+        }
+    }
+}
